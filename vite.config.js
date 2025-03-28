cea85353602e6edc465a8951ceeb0dad14958e4e
조시헌 <56191064+joesiheon496@users.noreply.github.com>
2025-03-28 10:51:57 +0900

    fixed_1

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/ai-portfolio/', // ✅ GitHub Pages용 경로
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
